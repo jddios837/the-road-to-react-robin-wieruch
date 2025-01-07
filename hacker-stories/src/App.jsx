964c9f0a2870355a162d51121a60203430c69d34
jddios837 <jddios837@gmail.com>
2025-01-06 19:25:42 -0500

    Starting to understant basic component

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,24 +1,57 @@
-import * as React from 'react'
-
 function App() {
-    // you can do something in between
-    const title = 'React';
-
     const welcome = {
         greeting: 'Hey',
         title: 'React',
     }
 
+    const listObj = [
+        { title: 'Item 1', url: 'https://google.com', author: 'Author 1', num_comments: 10, points: 100, objectID: 1 },
+        { title: 'Item 2', url: 'https://google.com', author: 'Author 2', num_comments: 20, points: 200, objectID: 2 },
+        { title: 'Item 3', url: 'https://google.com', author: 'Author 3', num_comments: 30, points: 300, objectID: 3 },
+        { title: 'Item 4', url: 'https://google.com', author: 'Author 4', num_comments: 40, points: 400, objectID: 4 },
+    ]
+
     function getTitle(title) {
         return "Get Title: " + title;
+    }
+
+    function Search() {
+        return (
+            <div>
+                <label htmlFor="search">Search: </label>
+                <input id="search" type="text"/>
+            </div>
+        );
+    }
+
+    function List() {
+        return (
+            <ul>
+            {listObj.map((item) => {
+                    return (
+                        <li key={item.objectID}>
+                        <span>
+                            <a href={item.url}>{item.title}</a>
+                        </span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                        </li>
+                    );
+                })}
+            </ul>
+        );
     }
 
     return (
         <div>
             <h1>{welcome.greeting} {getTitle(welcome.title)}</h1>
 
-            <label htmlFor="search">Search: </label>
-            <input id="search" type="text"/>
+            <Search />
+
+            <hr/>
+
+            <List />
         </div>
     )
 }
